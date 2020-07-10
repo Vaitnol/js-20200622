6f3bd14c517f05ceff2a6f1f54af921cf3f33f0a
Volodymyr Shevchuk <dosandkv@gmail.com>
2020-07-10 11:18:35 +0300

    Added solutions for "dom-document-loading";

--- 02-objects-arrays-intro-to-testing/3-trim-symbols/solution/index.js
+++ 02-objects-arrays-intro-to-testing/3-trim-symbols/solution/index.js
@@ -1,11 +1,12 @@
 /**
  * trimSymbols - removes consecutive identical symbols if they quantity bigger that size
  * @param {string} string - the initial string
- * @param {number} size - the allowed size of consecutive identical symbols
+ * @param {number | undefined} size - the allowed size of consecutive identical symbols
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
-  if (!size) return '';
+  if (size === 0) return '';
+  if (size === 'undefined') return string;
 
   const firstSlice = string.slice(0, size);
   const rest = [...string.slice(size)];
---
+++ 04-dom-document-loading/module-plan.md
@@ -0,0 +1,16 @@
+# Plan for Module #4 dom-document-loading
+
+## Проверка ДЗ
+
+* ColumnChart
+
+## Новая тема
+
+* прототипное наследование в JS
+* наследование классов, использование extends
+* загрузка документа, каким образом браузер читает html
+* Javascript modules - детальный обзор
+* Коллекции Map и Set
+* тесты - введение
+
+## Разбор ДЗ - "SortableTable" и "NotificationMessage" компонент
